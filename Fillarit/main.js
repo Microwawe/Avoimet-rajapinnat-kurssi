db478da0e981e54650eedbc9cfa144b9e6e6871e
Unknown <microvvavve@gmail.com>
2018-02-06 11:38:11 +0200

    Teht 4 tehty

--- Fillarit/main.js
+++ Fillarit/main.js
@@ -2,18 +2,9 @@ let map;
 let infowindow;
 
 function showStations(stations) {
-    /*
-    for(let station of stations) {
-        const data = `
-        Name: ${station.name}
-        Bikes: ${station.free_bikes} / ${station.empty_slots}
-        Location: ${station.latitude}, ${station.longitude}`;
-    }  
-    */  
-
-    for(station of stations) {
+    for (let station of stations) {
         const marker = new google.maps.Marker({
-            position: {lat: station.latitude, lng: station.longitude},
+            position: { lat: station.latitude, lng: station.longitude },
             map: map
         });
         const content = `<h4>${station.name}</h4> <br> <p>Pyöriä tällä asemalla: ${station.free_bikes} / ${station.empty_slots}</p>`;
@@ -22,6 +13,41 @@ function showStations(stations) {
             infowindow.open(map, marker);
         })
     }
+}
+
+function formatWeatherData(data) {
+    console.log(data);
+    const weatherIcon = document.querySelector('.weather-icon');
+    const windIcon = document.querySelector('.wind-icon');
+    const windText = document.querySelector('.wind-text');
+    const temperature = document.querySelector('.degrees');
+    const weatherDesc = document.querySelector('.description');
+    const degrees = data.main.temp;
+    const description = data.weather[0].description;
+    const windSpeed = data.wind.speed;
+    const windDir = data.wind.deg + 180;
+
+    temperature.innerHTML = `${degrees}&ordm;C`;
+    weatherDesc.textContent = description;
+
+    windText.textContent = `${windSpeed} m/s`;
+    windIcon.style.transform = `rotate(${windDir}deg)`;
+    weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherIcon.alt = data.weather[0].main;
+
+
+}
+
+
+
+
+
+
+
+function getWeather() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?id=658226&lang=fi&units=metric&APPID=bd5e378503939ddaee76f12ad7a97608")
+        .then(response => response.json())
+        .then(data => formatWeatherData(data));
 }
 
 function getData() {
@@ -39,4 +65,5 @@ function initMap() {
     infowindow = new google.maps.InfoWindow();
 
     getData();
+    getWeather();
 }
